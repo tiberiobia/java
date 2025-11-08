ed6afe2ece47dc18f2d9dbe22e445ef37f3bfa70
tiberiobia <tiberiobia09@gmail.com>
2025-11-08 14:04:29 +0000

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int largura = 10;
+        int altura = 20;
+        int area = altura * largura;
+        System.out.println("A area do retangulo é: " + area);
+    }
+}
